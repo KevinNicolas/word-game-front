5132df1b3f664bc76998a4d91fe83b2fb414f7a0
Kevin-Leguiza-Gaggero <nicolaskevinleguiza@gmail.com>
2022-03-24 01:58:49 -0300

    feat: start project

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import App from './App'
+
+// import { useTheme } from './hooks/useTheme'
+// import { GlobalStyle, DarkTheme, LightTheme } from './assets/styles'
+
+const Main = () => {
+  // const [theme, toggleTheme] = useTheme()
+  // document.addEventListener('toggle-theme', toggleTheme)
+  return (
+    <span>Home</span>
+    // <>
+    //   <GlobalStyle />
+    //   {
+    //     theme === 'dark'
+    //       ? <DarkTheme />
+    //       : <LightTheme />
+    //   }
+    //   <App />
+    // </>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
