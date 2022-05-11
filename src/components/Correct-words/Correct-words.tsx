de884281a6f38bd9663ddb7d4c1d3abf843442e8
Kevin-Leguiza-Gaggero <nicolaskevinleguiza@gmail.com>
2022-05-11 15:32:02 -0300

    fix: remove 'index' key en 'LetterData'

--- src/components/Correct-words/Correct-words.tsx
+++ src/components/Correct-words/Correct-words.tsx
@@ -16,8 +16,8 @@ export const CorrectWords = () => {
       const newLetterToDisplay: LetterData[] = []
       enteredWord.split('').forEach((letter: string, index: number) => {
         if (lettersToDisplay[index].status === 'correct') return newLetterToDisplay.push(lettersToDisplay[index])
-        if (letter === guessWord[index]) return newLetterToDisplay.push({ letter, index, status: 'correct' })
-        return newLetterToDisplay.push({ letter: '_', index, status: 'normal' })
+        if (letter === guessWord[index]) return newLetterToDisplay.push({ letter, status: 'correct' })
+        return newLetterToDisplay.push({ letter: '_', status: 'normal' })
       })
       setlettersToDisplay(newLetterToDisplay)
     }
--- src/utils/parse-word/parse-word.ts
+++ src/utils/parse-word/parse-word.ts
@@ -7,11 +7,11 @@ export const parseWord = ({ enteredWord, wordToGuess }: Props): { wordIsComplete
   let correctLetterCounter: number = 0
 
   enteredWord.split('').forEach((letter: string, index: number): void => {
-    if (!wordToGuess.includes(letter)) { lettersData.push({ index, letter, status: 'unexist' }); return }
+    if (!wordToGuess.includes(letter)) { lettersData.push({ letter, status: 'existInWord' }); return }
 
-    if (wordToGuess[index] === letter) { lettersData.push({ letter, index, status: 'correct' }); correctLetterCounter++; return }
+    if (wordToGuess[index] === letter) { lettersData.push({ letter, status: 'correct' }); correctLetterCounter++; return }
 
-    lettersData.push({ letter, index, status: 'incorrect' })
+    lettersData.push({ letter, status: 'incorrect' })
   })
 
   return { wordIsComplete: correctLetterCounter === wordToGuess.length, lettersData }
